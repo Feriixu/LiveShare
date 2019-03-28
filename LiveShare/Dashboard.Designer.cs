065d6975706fa3b415fd0b7ccca0cb3168c73328
Feriixu <feriixu@users.noreply.github.com>
2019-03-28 13:23:06 +0100

    Initial commit

---
+++ LiveShare/Dashboard.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace LiveShare
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.RichTextBox();
+            this.textBoxServerIP = new System.Windows.Forms.TextBox();
+            this.textBoxServerPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxClientIP = new System.Windows.Forms.TextBox();
+            this.textBoxClientPort = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonListen = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.taskReceiveText = new System.ComponentModel.BackgroundWorker();
+            this.taskSendText = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(12, 94);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(776, 344);
+            this.textBox.TabIndex = 0;
+            this.textBox.Text = "";
+            this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            this.textBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // textBoxServerIP
+            // 
+            this.textBoxServerIP.Location = new System.Drawing.Point(41, 14);
+            this.textBoxServerIP.Name = "textBoxServerIP";
+            this.textBoxServerIP.Size = new System.Drawing.Size(100, 20);
+            this.textBoxServerIP.TabIndex = 1;
+            // 
+            // textBoxServerPort
+            // 
+            this.textBoxServerPort.Location = new System.Drawing.Point(41, 41);
+            this.textBoxServerPort.Name = "textBoxServerPort";
+            this.textBoxServerPort.Size = new System.Drawing.Size(100, 20);
+            this.textBoxServerPort.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "IP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Port:";
+            // 
+            // textBoxClientIP
+            // 
+            this.textBoxClientIP.Location = new System.Drawing.Point(41, 14);
+            this.textBoxClientIP.Name = "textBoxClientIP";
+            this.textBoxClientIP.Size = new System.Drawing.Size(100, 20);
+            this.textBoxClientIP.TabIndex = 1;
+            // 
+            // textBoxClientPort
+            // 
+            this.textBoxClientPort.Location = new System.Drawing.Point(41, 41);
+            this.textBoxClientPort.Name = "textBoxClientPort";
+            this.textBoxClientPort.Size = new System.Drawing.Size(100, 20);
+            this.textBoxClientPort.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "IP:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Port:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonListen);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBoxServerIP);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBoxServerPort);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(240, 75);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Server";
+            // 
+            // buttonListen
+            // 
+            this.buttonListen.Location = new System.Drawing.Point(148, 14);
+            this.buttonListen.Name = "buttonListen";
+            this.buttonListen.Size = new System.Drawing.Size(86, 47);
+            this.buttonListen.TabIndex = 4;
+            this.buttonListen.Text = "Start Listener";
+            this.buttonListen.UseVisualStyleBackColor = true;
+            this.buttonListen.Click += new System.EventHandler(this.buttonListen_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.buttonConnect);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.textBoxClientIP);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.textBoxClientPort);
+            this.groupBox2.Location = new System.Drawing.Point(259, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(240, 75);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Client";
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(147, 14);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(86, 47);
+            this.buttonConnect.TabIndex = 4;
+            this.buttonConnect.Text = "Connect to Server";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // taskReceiveText
+            // 
+            this.taskReceiveText.DoWork += new System.ComponentModel.DoWorkEventHandler(this.taskReceiveText_DoWork);
+            // 
+            // taskSendText
+            // 
+            this.taskSendText.DoWork += new System.ComponentModel.DoWorkEventHandler(this.taskSendText_DoWork);
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox);
+            this.Name = "Dashboard";
+            this.Text = "LiveShare Text";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox textBox;
+        private System.Windows.Forms.TextBox textBoxServerIP;
+        private System.Windows.Forms.TextBox textBoxServerPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxClientIP;
+        private System.Windows.Forms.TextBox textBoxClientPort;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonListen;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.ComponentModel.BackgroundWorker taskReceiveText;
+        private System.ComponentModel.BackgroundWorker taskSendText;
+    }
+}
+
---
+++ LiveShare/Dashboard.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+namespace LiveShare
+{
+    public partial class Dashboard : Form
+    {
+        private TcpClient client;
+        private StreamReader STR;
+        private StreamWriter STW;
+        private string received;
+        private string textToSend;
+
+        public Dashboard()
+        {
+            InitializeComponent();
+            textBoxServerIP.Text = GetLocalIPAddress();
+            textBoxClientIP.Text = GetLocalIPAddress();
+            textBoxClientPort.Text = "59308";
+            textBoxServerPort.Text = "59308";
+        }
+
+        private void buttonListen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                TcpListener listener = new TcpListener(IPAddress.Any, int.Parse(textBoxServerPort.Text));
+                listener.Start();
+                client = listener.AcceptTcpClient();
+                STR = new StreamReader(client.GetStream());
+                STW = new StreamWriter(client.GetStream())
+                {
+                    AutoFlush = true
+                };
+                MessageBox.Show($"Client connected: {client.Client.RemoteEndPoint.ToString()}");
+                taskReceiveText.RunWorkerAsync();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error accepting Client.\n\n{ex.Message}");
+            }
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            client = new TcpClient();
+            try
+            {
+                IPEndPoint IpEnd = new IPEndPoint(IPAddress.Parse(textBoxClientIP.Text), int.Parse(textBoxClientPort.Text));
+                client.Connect(IpEnd);
+                if (client.Connected)
+                {
+                    MessageBox.Show($"Client connected to {client.Client.RemoteEndPoint}");
+                    STR = new StreamReader(client.GetStream());
+                    STW = new StreamWriter(client.GetStream())
+                    {
+                        AutoFlush = true
+                    };
+                    taskReceiveText.RunWorkerAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Format("Can't connect:\n\n{0}", ex.Message.ToString()), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void taskReceiveText_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (client.Connected)
+            {
+                try
+                {
+                    received = STR.ReadLine();
+                    //MessageBox.Show($"Received: {received}");
+
+                    textBox.Invoke(new MethodInvoker(delegate ()
+                    {
+                        textBox.Text = received;
+                    }));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Connection probably lost.\n{ex.Message}");
+                }
+            }
+            MessageBox.Show("Connection Lost");
+        }
+
+        private void textBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((client != null) ? client.Connected : false)
+            {
+                taskSendText.RunWorkerAsync();
+            }
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            //if ((client != null) ? client.Connected : false)
+            //{
+            //    textToSend = textBox.Text;
+            //    taskSendText.RunWorkerAsync();
+            //}
+        }
+
+        private void taskSendText_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if ((client != null) ? client.Connected : false)
+            {
+                //MessageBox.Show($"Writing to Stream: {textToSend}");
+                textBox.Invoke(new MethodInvoker(delegate ()
+                {
+                    textToSend = textBox.Text;
+                }));
+                STW.WriteLine(textToSend);
+            }
+        }
+    }
+}
